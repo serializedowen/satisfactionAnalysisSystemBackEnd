826133dc4fc31396b59762af60ab8ea12186cc7f
sunmingming <102798281@qq.com>
2020-04-21 18:31:24 +0800

    init

---
+++ satisfactionAnalysisSystemBackEnd/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for satisfactionAnalysisSystemBackEnd project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'satisfactionAnalysisSystemBackEnd.settings')
+
+application = get_wsgi_application()
