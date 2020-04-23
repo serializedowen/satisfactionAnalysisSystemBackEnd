fe7201bb6333c7880c89cfbe67aa222a601af8a7
sunmingming <102798281@qq.com>
2020-04-23 17:33:48 +0800

    数据库连接配置

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    uid=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=20, verbose_name='用户名')
+    username=models.CharField(max_length=20,unique=True,verbose_name='账号') # 用户名唯一
+    password=models.CharField(max_length=12,verbose_name='密码')
+    def __str__(self):
+        return self.username
+
--- satisfactionAnalysisSystemBackEnd/settings.py
+++ satisfactionAnalysisSystemBackEnd/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'satisfactionAnalysisSystemBackEnd.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'satisfactonAnalysis',
+        'USER': 'root',
+        'PASSWORD': '2211smm..',
+        'HOST': '118.126.110.182',
+        'PORT': 3306
     }
 }
 
